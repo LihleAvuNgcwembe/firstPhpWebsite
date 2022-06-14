6d26ef4b9b6561085c63446a2c5687ad56fdcb68
LihleAvuNgcwembe <107505046+LihleAvuNgcwembe@users.noreply.github.com>
2022-06-14 22:13:40 +0200

    added files

---
+++ shopping-cart.js
@@ -0,0 +1,12 @@
+<section class="shop container">
+	<h2 class="section-title">Shop Products</h2>
+	
+	<div class="shop-content">
+		<div class = "product-box">
+			<img src ="img/cabbage.jpg" alt="" class="product-img">
+			<h2 class="product-title">Cabbage</h2>
+			<span class="price">R10</span>
+			<i class='fas fa-shopping-bag'></i>
+		</div>
+	</div>
+</section>
